e1a5acdcb901e139368c3add0998a931e4de4a63
pivnati <natanmuleta911@gmail.com>
2024-08-31 22:50:25 -0700

    modify the view

---
+++ resources/views/products/show.blade.php
@@ -0,0 +1,15 @@
+@extends('layout') <!-- Ensure you have a layout file -->
+
+@section('content')
+    <h1>{{ $product->name }}</h1>
+    <p>{{ $product->description }}</p>
+    <p>Price: ${{ $product->price }}</p>
+
+    <form action="{{ route('orders.store') }}" method="POST">
+        @csrf
+        <input type="hidden" name="product_id" value="{{ $product->id }}">
+        <label for="quantity">Quantity:</label>
+        <input type="number" name="quantity" min="1" value="1">
+        <button type="submit">Order</button>
+    </form>
+@endsection
