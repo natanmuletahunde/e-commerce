eef26c1e06facb8677bf156884dc89db3e03bf70
pivnati <natanmuleta911@gmail.com>
2024-08-31 23:15:41 -0700

    modify the router

--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,9 @@ Route::get('/products/{product}', [ProductController::class, 'show'])->name('pro
 Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
 
 Route::middleware(['auth'])->group(function () {
-Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
-});
+Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');});
+Route::middleware(['auth'])->group(function () {
+Route::get('/products/create', [ProductController::class, 'create'])->name('products.create');
+Route::post('/products', [ProductController::class, 'store'])->name('products.store');});
+Route::get('/', [ProductController::class, 'index'])->name('products.index');
+Route::get('/products/{product}', [ProductController::class, 'show'])->name('products.show');
